923b96b0d8eb19478bc4489c792fde222cd7a84a
Jin-Moa <wlone825@gmail.com>
2023-09-08 16:31:51 +0530

    in progress

---
+++ soruce_code.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+int main() {
+    int num = 123, reverse_num;
+    
+    /*printf("Enter the number which you would like to print in the reverse order : ");
+    scanf("%d", &num); If we would like to take an input */
+
+    reverse_num += num % 100;
+
+
+}
